511635d2cfb94335d8e978c6f134e4ae58bde262
D1di3r-collab <jeffdekiller85@gmail.com>
2026-04-28 12:01:35 -0500

    feat(data-model): created the database tables in the package model

---
+++ app/src/main/java/com/example/finanzasapp/data/model/Expense.kt
@@ -0,0 +1,20 @@
+package com.example.finanzasapp.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "expenses")
+data class Expense(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+
+    val userId: Long,
+    val amount: Double,
+    val category: ExpenseCategory,
+    val description: String,
+
+    val date: Long = System.currentTimeMillis(),
+    val isRecurring: Boolean = false
+)
--- app/src/main/java/com/example/finanzasapp/data/model/ExpenseCategory.kt
+++ app/src/main/java/com/example/finanzasapp/data/model/ExpenseCategory.kt
@@ -1,4 +1,15 @@
 package com.example.finanzasapp.data.model
 
-enum class ExpenseCategory {
+
+enum class ExpenseCategory (val  displayName: String){
+    FOOD("Alimentación"),
+    TRANSPORT("Transporte"),
+    ENTERTAINMENT( "Entretenimiento"),
+    BILLS( "Servicios"),
+    SHOPPING( "Compras"),
+    HEALTH( "Salud"),
+    EDUCATION("Educación"),
+    TRAVEL("Viajes"),
+    OTHER( "Otros")
+
 }
