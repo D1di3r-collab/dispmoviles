9a6d04b78a5dd196a5364f76acdc54ed920202e9
D1di3r-collab <jeffdekiller85@gmail.com>
2026-05-05 11:36:35 -0500

    feat(ui): Color.kt and theme.kt created whit color and theme settings

--- app/src/main/java/com/example/finanzasapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/finanzasapp/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.example.finanzasapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+
+val PurpleGrey80 = Color(0xFFD0BCFF)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple40 = Color(0xFF6650A4)
+val PurpleGrey40 = Color(0xFF625B71)
+val Pink40 = Color(0xFFF7D5260)
+
+//Colores Principales - Tema financiero
+val PrimaryGreen = Color(0xFF00C853)
+val PrimaryGreenDark = Color(0xFF00A043)
+val SecondaryBlue = Color(0xFF2196F3)
+val AccentOrange = Color(0xFFFF9800)
+
+// Color de fondo
+val BackgroundLight = Color(0xFFF6F8FA)
+val SurfaceLight = Color(0xFFFFFFFF)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
